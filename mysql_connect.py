74a7975e65b306051fddf0b3d7d055e78c68299d
Ayush_S_Acharya <115517887+AyushAcharya07@users.noreply.github.com>
2023-10-19 09:38:44 +0530

    Update mysql_connect.py

--- mysql_connect.py
+++ mysql_connect.py
@@ -14,13 +14,17 @@ for i in range(n):
   name=input("Enter the name : ")
   var2="INSERT INTO data VALUES("+str(roll)+",'"+name+"')"
   func.execute(var2)
-  print(func.rowcount, "record inserted.")'''
+  print(func.rowcount, "record inserted.")
   
 info=input("Enter the roll no to be deleted : ")
 var3="DELETE FROM data WHERE Roll_No="+info
-func.execute(var3)
-p.commit()
-print(func.rowcount, "record deleted.")
+func.execute(var3)'''
 
+name=input("Enter the new name to be inserted : ")
+roll=input("Enter the roll no to be deleted : ")
+var4="UPDATE data SET Name='"+name+"' WHERE Roll_no="+roll
+func.execute(var4)
+p.commit()
+print(func.rowcount, "record updated.")
 
 
